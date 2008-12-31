6b89f70d65f7b96ca533c93b01a3f7c4fc505d54
Phillip Toland <ptoland@thehive.com>
2008-12-31 10:57:56 -0600

    Added SNMP tasks in a separate file.

---
+++ lib/erlbox/snmp.rb
@@ -0,0 +1,23 @@
+## -------------------------------------------------------------------
+##
+## Erlang Toolbox: SNMP Tasks
+## Copyright (c) 2008 The Hive.  All rights reserved.
+##
+## -------------------------------------------------------------------
+
+MIB_SRCS = FileList["mibs/MAGKV-MIB.mib"]
+MIB_BINS = MIB_SRCS.pathmap("%{mibs,priv/mibs}X.bin")
+
+directory 'priv/mibs'
+
+CLEAN.include %w( priv/mibs )
+
+rule ".bin" => ["%{priv/mibs,mibs}X.mib"] do |t|
+  puts "compiling #{t.source}..."
+  sh "erlc -I priv/mibs -o priv/mibs #{t.source}", :verbose => false
+end
+
+desc "Compile SNMP mibs to .bin files"
+task :compile_mibs => ['priv/mibs'] + MIB_BINS
+
+task :compile => [:compile_mibs]
